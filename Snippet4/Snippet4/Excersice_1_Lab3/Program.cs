1f5e1ee180e70c07d7fad877ccb2aeb7b3284687
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2020-06-08 23:14:54 +0700

    .

---
+++ Snippet4/Snippet4/Excersice_1_Lab3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersice_1_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Atomic Infomation\n ====================\n");
+            Atom a = new Atom();
+            a.creatAtom();
+            a.GetFullInfor();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Snippet4/Snippet4/Lab3/ListBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class ListBox : Window
+    {
+        private string listBoxContents;
+        public ListBox(int top, int left, string contents) : base(top, left)
+        {
+            listBoxContents = contents;
+        }
+
+        public override void DrawWindow()
+        {
+            {
+                base.DrawWindow();
+                Console.WriteLine("Writing string to the listbox: {0}", listBoxContents);
+            }
+        }
+    }
+}
